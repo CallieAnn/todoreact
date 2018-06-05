c57b8efebb140a65482e85c919ee009c06c29b03
CallieAnn <dedorec@my.lanecc.edu>
2018-06-04 23:08:16 -0700

    form shows up on page, but doesn't yet update state when task is entered

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import TaskList from './TaskList';
+import TaskForm from './TaskForm';
 
 class App extends Component {
   constructor(props) {
@@ -13,8 +14,7 @@ class App extends Component {
         {task: 'Do Gardening', isComplete: true},
         {task: 'Renew Library Account', isComplete: false},
       ],
-      formInput: JSON.parse(localStorage.getItem('TASKS'))
-    
+      formInput: JSON.parse(localStorage.getItem('TASKS')),
       }
       
 
@@ -56,6 +56,7 @@ class App extends Component {
       parentDiv.classList.remove('has-error');
     console.log("Add the task");
   }
+}
   
   render() { 
       localStorage.setItem('TASKS', JSON.stringify(this.state.tasks))
@@ -65,6 +66,7 @@ class App extends Component {
       <div className="container">
         <TaskList tasks={this.state.tasks} formInput={this.state.formInput} toggleTaskStatus={this.toggleTaskStatus} 
         deleteTask={this.deleteTask}/>
+        <TaskForm tasks={this.state.tasks} addTask={this.addTask}/>
       </div>
     )    
   }
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,7 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TaskForm extends Component {
+class TaskForm extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = { task: '' };
+
+        this.inputUpdated = this.inputUpdated.bind(this); 
+    }
+     
+    inputUpdated(e) { 
+        const {value} = e.target; 
+        this.setState( {task:value} 
+        ); 
+    }
+
+    render() {
+        
+
+        return (
+            <div className="row input-area">
+                <form onSubmit={this.submitTask}>
+                    <div className="col-md-1">
+                        <label htmlFor="task">Task:</label>
+                    </div>
+                    <div className="form-group col-md-10">
+                        <input className="form-control"
+                            type="input" name="task" 
+                            value={this.state.task}
+                            onInput={this.inputUpdated}
+                        />
+                    </div>
+                    <div className="form-group col-md-1">
+                        <button type="submit" className="btn btn-success"> Add Task </button>
+                    </div>
+                    <div className="col-md-1"></div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default TaskForm;
